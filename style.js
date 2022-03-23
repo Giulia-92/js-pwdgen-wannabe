d8cef8baaab6db7696b6309e4aa2107275415398
Giulia-92 <cionco.giulia@gmail.com>
2022-03-23 16:04:38 +0100

    js-pwdgen-wannabe

---
+++ style.js
@@ -0,0 +1,12 @@
+var userName = prompt('Quale è il tuo nome?');
+console.log(userName);
+
+
+var surName = prompt ('Quale è il tuo cognome?');
+console.log(userName);
+
+
+var colorPrefer = prompt ('Quale è il tuo colore preferito?');
+console.log(colorPrefer);
+
+document.getElementById('nome').innerHTML 
